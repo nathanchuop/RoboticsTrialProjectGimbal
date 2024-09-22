c73d47f228206fd8a9d9a6fe50ebeb4474e3452b
nathanchuop <nathan.chuop@sjsu.edu>
2024-09-21 17:47:22 -0700

    Added Function to Initialize MPU6050

--- OneAxisGimbal/OneAxisGimbal.ino
+++ OneAxisGimbal/OneAxisGimbal.ino
@@ -1,10 +1,49 @@
+#include <Servo.h> //adds the servo library to control servo
+#include <Wire.h> //adds wire library to communicate with I2C device (mpu6050)
+Servo servo; //creates a servo object
+
+void initialize_mpu6050(){
+  //turn on mpu6050
+  Wire.beginTransmission(0x68); //call Wire library to link arduino with mpu (0x68 is the address of the mpu6050)
+  Wire.write(0x6B); //selects power management register of the mpu so I can configure it
+  Wire.write(0x00); //turns on the mpu6050
+  Wire.endTransmission(); //stops communication with current register & sends out the .write commands
+
+  //Select sensitivity of Accelerometer (measure linear movement/position on a 3d axis)
+  Wire.beginTransmission(0x68); 
+  Wire.write(0x1C); //accelerometer configuration register
+  Wire.write(0x8); //selects sensitivity of accelerometer (+-4g)
+  Wire.endTransmission();
+
+  //Select sensitivity of Gyroscope (measures angular velocity around an axis)
+  Wire.beginTransmission(0x68); 
+  Wire.write(0x1B); //gyroscope configuration register
+  Wire.write(0x08); //selects sensitivity of gyroscope (500 deg/s)
+  Wire.endTransmission();
+}
+
+
+
 void setup() {
-  // put your setup code here, to run once:
-  //test changes
-  //test changes again x2
+  servo.attach(9); //sets digital pin9 as the location for the servo object
+  servo.write(0); //reset servor to 0 degrees
+  delay(1000);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  servo.write(0);
+  delay(1000);
+  servo.write(180);
+  delay(1000);
 
 }
+
+
+
+
+
+
+
+
+
